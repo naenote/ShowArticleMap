0fa1eb5f9a9fe797777eaf4c5529443d5e7ed0f4
naenote <32070398+naenote@users.noreply.github.com>
2018-04-25 21:31:45 +0900

    v0.4 updates
    全体的なコード整形、Cocoon対応

--- showArticleMap.php
+++ showArticleMap.php
@@ -10,73 +10,73 @@ License: GPL2
 */
 
 /*  Copyright 2017 NAE (email : @__NAE__)
- 
+
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License, version 2, as
      published by the Free Software Foundation.
- 
+
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
- 
+
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-
-function nae_insert_str($text, $insert, $num){
+function nae_insert_str($text, $insert, $num)
+{
     $returnText = $text;
-    $text_len = mb_strlen($text, "utf-8");
-    $insert_len = mb_strlen($insert, "utf-8");
-    for($i=0; ($i+1)*$num<$text_len; $i++) {
-        $current_num = $num+$i*($insert_len+$num);
+    $text_len = mb_strlen($text, 'utf-8');
+    $insert_len = mb_strlen($insert, 'utf-8');
+    for ($i = 0; ($i + 1) * $num < $text_len; ++$i) {
+        $current_num = $num + $i * ($insert_len + $num);
         $returnText = preg_replace("/^.{0,$current_num}+\K/us", $insert, $returnText);
     }
+
     return $returnText;
 }
 
-function nae_get_dataset (){
+function nae_get_dataset()
+{
     $args_post = [
         'posts_per_page' => -1,
-        'post_type'        => 'post',
-        'post_status'      => 'publish'
+        'post_type' => 'post',
+        'post_status' => 'publish',
     ];
     $args_page = [
         'posts_per_page' => -1,
-        'post_type'        => 'page',
-        'post_status'      => 'publish'
+        'post_type' => 'page',
+        'post_status' => 'publish',
     ];
-    $post_array = get_posts( $args_post );
-    $page_array = get_pages( $args_page );
+    $post_array = get_posts($args_post);
+    $page_array = get_pages($args_page);
     $articles = array_merge($post_array, $page_array);
     $nodes = [];
     $edges = [];
 
     foreach ($articles as $post) {
         $category = get_the_category($post->ID);
-        
-        // print($post->post_name ."|". $category[0]->cat_ID."<br>");
-        $ancestors_cat_IDs = get_ancestors( $category[0]->cat_ID, 'category' );
-        if(empty($ancestors_cat_IDs)) {
+        $ancestors_cat_IDs = get_ancestors($category[0]->cat_ID, 'category');
+        if (empty($ancestors_cat_IDs)) {
             $root_category_ID = $category[0]->cat_ID;
         } else {
             $root_category_ID = array_pop($ancestors_cat_IDs);
         }
         $root_category = get_category($root_category_ID);
-        $group_name = !empty($category) ? $root_category->slug : "固定ページ";
+        $group_name = !empty($category) ? $root_category->slug : '固定ページ';
 
         $nodes[] = [
             'id' => $post->ID,
-            'label' => nae_insert_str(urldecode($post->post_name), "\r\n", 20),
+            'label' => nae_insert_str(urldecode($post->ID.':'.$post->post_name), "\r\n", 20),
             'group' => urldecode($group_name),
-            'title' => '<a href="'.get_permalink($post).'" target="_blank">'.$post->post_title.'</a>'
+            'title' => '<a href="'.get_permalink($post).'" target="_blank">'.$post->post_title.'</a>',
         ];
-        $post_content = str_replace("[show_article_map]","",$post->post_content); 
-        $html = apply_filters( 'the_content', $post_content );
+        $post_content = str_replace('[show_article_map]', '', $post->post_content);
+        $html = apply_filters('the_content', $post_content);
 
-        $dom = new DOMDocument;
+        $dom = new DOMDocument();
         @$dom->loadHTML(mb_convert_encoding($html, 'HTML-ENTITIES', 'UTF-8'));
         $xpath = new DOMXPath($dom);
 
@@ -89,19 +89,21 @@ function nae_get_dataset (){
         foreach ($xpath->query($query) as $node) {
             $href = $xpath->evaluate('string(@href)', $node);
             $linked_post_id = url_to_postid($href);
-            if ($linked_post_id != 0 && !in_array(['from'=>$post->ID,'to'=>$linked_post_id],$edges )){
+            if (0 != $linked_post_id && !in_array(['from' => $post->ID, 'to' => $linked_post_id], $edges)) {
                 $edges[] = [
                     'from' => $post->ID,
-                    'to' => $linked_post_id
+                    'to' => $linked_post_id,
                 ];
             }
         }
     }
-    return "[".json_encode($nodes).",".json_encode($edges)."]";
+
+    return '['.json_encode($nodes).','.json_encode($edges).']';
 }
 
-function nae_echo_article_map(){
-    $dataset = nae_get_dataset ();
+function nae_echo_article_map()
+{
+    $dataset = nae_get_dataset();
     $body = <<<EOD
     <div>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/vis/4.20.0/vis.min.js"></script>
@@ -146,8 +148,7 @@ function nae_echo_article_map(){
             // double click node to open an article
             network.on('doubleClick', function(e){
                 var nodeID = e.nodes.toString();
-                var url = jQuery(data.nodes.get(nodeID)['title']).attr('href');
-                //console.log(jQuery(data.nodes.get(nodeID)['title'])); console.log(url);
+                var url = jQuery(data.nodes.get(nodeID).title).attr('href');
                 window.open(url,'_blank');
             });
             
@@ -195,7 +196,6 @@ function nae_echo_article_map(){
                 jQuery("#groupList :checkbox:checked").each(function(){
                    visibleGroupNames.push(this.value);
                 });
-                // console.log("visibleGroupNames:"+visibleGroupNames);
                 var diffGroupNames = diffArray(currentGroupNames,visibleGroupNames);
                 if(currentGroupNames.length < visibleGroupNames.length){
                     for(i=0;i<diffGroupNames.length;i++){
@@ -223,7 +223,8 @@ function nae_echo_article_map(){
         </script>
     </div>
 EOD;
+
     return $body;
 }
 
-add_shortcode( 'show_article_map', 'nae_echo_article_map' );
+add_shortcode('show_article_map', 'nae_echo_article_map');
